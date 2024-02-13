b0d1a782eae0fd0a59ccbc29429aac8381f20c56
mmcgrath2 <mmcgrath1@mines.edu>
2024-02-12 18:59:15 -0700

    Updated the Student class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,36 @@
+import java.io.*;
+import java.util.*;
 
-public class Student {
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	
+	public Student(String name, int score) {
+		super();
+		this.name = name;
+		this.score = score;
+	}
 
+
+	@Override
+	public int compareTo(Student o) {
+		if (this.score < o.getScore()){
+			return o.getScore();
+		} else {
+			return this.score;
+		}
+	}
+	
+	public int getScore() {
+		return score;
+	}
+
+
+	@Override
+	public String toString() {
+		return "" + name + " " + score;
+	}
+	
+	
 }
